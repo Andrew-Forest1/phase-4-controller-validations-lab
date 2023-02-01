a262498c72a869fb3afa36e31c37746750931ac7
Andrew <aforest41@yahoo.com>
2023-02-01 10:50:19 -0800

    commit tree 1f6e27f16201aa4f76a0c569e2cf939d2a5116d6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+
+    private
+
+    def handle_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}
+    end
 end
--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,17 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
-    render json: author, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+    
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+
+    # if author.valid?
+    #   render json: author, status: :created
+    # else
+    #   render json: {errors: "Invalid data - could not create author"}, status: :unprocessable_entity
+    # end
   end
 
   private
